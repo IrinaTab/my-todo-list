cd4d42e19ca31b031ebad4e8cc6908216b52f279
IrinaTab <irina.mariya.anatoliy@gmail.com>
2025-04-28 22:11:52 +0300

    коммент

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import "./styles.css"
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 import TodoList from "./components/TodoList"; 
 
 
 function App() {
   return (
     <div className="App center">
-      <header className="App-header">
+      <Router>
 
-        <TodoList />
+        <ScrollToTop />
 
-      </header>
+        <header className="App-header">
+
+          <TodoList />
+
+        </header>
+        
+      </Router>
+
     </div>
   );
 }
