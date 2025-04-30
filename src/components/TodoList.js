215c6e318917a69a081c26ca6c447c74a3170165
IrinaTab <irina.mariya.anatoliy@gmail.com>
2025-04-30 11:48:24 +0300

    сщхранение

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,38 +1,66 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
 import Calendar from 'react-calendar';
 import './styles.css'; // Импорт файла стилей
 
 import cross from "./../img/cross.svg"
 
-
 const TodoList = () => {
-    const [tasks, setTasks] = useState([]); // Массив для хранения задач
-    const [newTask, setNewTask] = useState(''); // Ввод новой задачи
-    const [selectedDate, setSelectedDate] = useState(null);
+  const [tasks, setTasks] = useState([]); // Массив для хранения задач
+  const [newTask, setNewTask] = useState(''); // Ввод новой задачи
+  const [selectedDate, setSelectedDate] = useState(null);
 
-    const tasksByDate = {};
-    tasks.forEach(task => {
-      if (!tasksByDate[task.date]) {
-        tasksByDate[task.date] = [];
-      }
-      tasksByDate[task.date].push(task);
-    });
-  
-    const handleInputChange = (event) => {
-      setNewTask(event.target.value);
-    };
-  
-    const handleAddTask = () => {
-      if (newTask.trim() !== '' && selectedDate) { // Добавляем проверку на selectedDate
-        const newTaskObj = { task: newTask, date: selectedDate };
-        setTasks([...tasks, newTaskObj]);
-        setNewTask('');
-      }
-    };
-    const handleDeleteTask = (index) => {
-        const updatedTasks = tasks.filter((_, i) => i !== index);
-        setTasks(updatedTasks);
-      };
+  // Функция для сохранения задач в localStorage
+  const saveTasksToLocalStorage = (tasks) => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  };
+
+  // Функция для загрузки задач из localStorage
+  const loadTasksFromLocalStorage = () => {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      return JSON.parse(tasks);
+    }
+    return [];
+  };
+
+  // Загружаем задачи из localStorage при инициализации компонента
+  useEffect(() => {
+    const initialTasks = loadTasksFromLocalStorage();
+    setTasks(initialTasks);
+  }, []);
+
+  // Сохраняем задачи в localStorage при изменении состояния tasks
+  useEffect(() => {
+    saveTasksToLocalStorage(tasks);
+  }, [tasks]);
+
+  const tasksByDate = {};
+  tasks.forEach(task => {
+    if (!tasksByDate[task.date]) {
+      tasksByDate[task.date] = [];
+    }
+    tasksByDate[task.date].push(task);
+  });
+
+  const handleInputChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== '' && selectedDate) { // Добавляем проверку на selectedDate
+      const newTaskObj = { task: newTask, date: selectedDate };
+      setTasks([...tasks, newTaskObj]);
+      setNewTask('');
+    }
+  };
+
+  const handleDeleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
 
     return (
       <div>
